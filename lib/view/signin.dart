28b86076b137c235a43ce0d2ca760b4a91b2111f
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2021-02-06 23:47:53 +0300

    Sign in screen is prepared

--- lib/view/signin.dart
+++ lib/view/signin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/widgets/widget.dart';
 
 class SignIn extends StatefulWidget {
   @override
@@ -8,6 +9,34 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            TextField(
+              style: TextStyle(color: Colors.black, fontSize: 20),
+              decoration: InputDecoration(hintText: "email"),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black, fontSize: 20),
+              decoration: InputDecoration(hintText: "password"),
+            ),
+            SizedBox(height: 8),
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              child: Container(
+                child: Text(
+                  "Forgot password?",
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Widget appBarMain(BuildContext context) {
+  return AppBar(
+    title: Text("flutter chat app"),
+    backgroundColor: Color(0xffaf00f0),
+  );
+}
